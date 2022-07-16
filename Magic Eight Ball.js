4eb6ab888c9b833f2b24a0cc2efb7800be24cb5a
Ashley Chea <60674422+ashleychea@users.noreply.github.com>
2022-07-15 20:20:58 -0400

    Update Magic Eight Ball.js

--- Magic Eight Ball.js
+++ Magic Eight Ball.js
@@ -1,14 +1,18 @@
-// created a variable called userName with a blank string.
+// Created a variable called userName with a blank string. Then set the variable userName to a string called Ashley. 
 let userName = ' ';
 userName = 'Ashley';
+
+// If the userName is assigned the console will print: Hello Ashley! If the userName variable has a blank string the console will only print: Hello!
 userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
 
+// Used an unchangable variable (const) to ask a question from the user to the Magic Eight Ball. Prints: Is Itally really beautiful?
 const userQuestion = 'Is Italy really beautiful?';
 console.log(userQuestion);
 
+// Created a constant variable named randomNumber to generate a random number from 0-7. 
 const randomNumber = Math.floor(Math.random() * 8);
 
-let eightBall = ' ';
+// Used a switch statement instead of if/else so that the code is easier to read and write for the user and participants. 
 switch(randomNumber){
     case 0:
         eightBall = 'It is certain'
@@ -35,4 +39,5 @@ switch(randomNumber){
         eightBall = 'Signs point to yes'
         break;
 }
+// Used a string interpolation (template literal) to print out the results. 
 console.log(`The eight ball says: ${eightBall}`);
